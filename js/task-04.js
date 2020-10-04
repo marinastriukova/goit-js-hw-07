a8a625cdf1a1699b5bc2bf7839e2c550ddfd8c10
Marina Striukova <marinastriukova@gmail.com>
2020-10-04 15:45:51 +0300

    4-5

---
+++ js/task-04.js
@@ -0,0 +1,35 @@
+const counter = {
+    value: 0,
+    increment() {    
+        this.value += 1;
+    },
+    decrement() {
+        this.value -= 1;
+    },
+};
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const valueEl = document.querySelector('#value');
+
+decrementBtn.addEventListener('click', function () {
+    counter.decrement();
+    valueEl.textContent = counter.value;
+});
+
+incrementBtn.addEventListener('click', function () {
+    counter.increment();
+    valueEl.textContent = counter.value;
+});
+
+// const decrementBtn = document.querySelector('[data-action="decrement"]');
+// const incrementBtn = document.querySelector('[data-action="increment"]');
+// const valueEl = document.querySelector('#value');
+
+// decrementBtn.addEventListener('click', function () {
+//     valueEl.textContent = Number(valueEl.textContent) - 1
+// });
+
+// incrementBtn.addEventListener('click', function () {
+//     valueEl.textContent = Number(valueEl.textContent) + 1
+// });
---
+++ js/task-05.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector('#name-input');
+let spanEl = document.querySelector('#name-output');
+
+function onInputChange(event) {
+    spanEl.textContent = event.target.value;
+    if(event.target.value === ''){
+        spanEl.textContent = 'незнакомец'
+    };
+
+}
+
+inputEl.addEventListener('input', onInputChange);
+
+  
